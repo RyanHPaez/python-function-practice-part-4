5c445244658fdbda11c0bda72ce971302d98feb5
RyanHPaez <solosnail@gmail.com>
2022-07-03 16:17:07 -0700

    hw2

---
+++ num_within.py
@@ -0,0 +1,8 @@
+def num_within(x,a,b):
+  return x in range(a,b+1)
+     
+print(num_within(2,2,2))     
+print(num_within(5,4,6))     
+print(num_within(3,4,8))
+print(num_within(1,5,50))
+print(num_within(10,1,20))
---
+++ rev_string.py
@@ -0,0 +1,6 @@
+def rev_string(my_str):
+  return my_str[::-1]
+
+print(rev_string("123"))
+print(rev_string("abc"))
+print(rev_string("cba"))
